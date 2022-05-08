b4d6d6a3a677801dc00ebce9a6cc66c14e293996
Ashwin V <77207196+AshDaGrat@users.noreply.github.com>
2022-05-08 22:05:50 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 #TODO: 
-# A better implimentation of Random_2_Generator()
-# Impliment a Game Over state
-# GUI 
-# Fix multiple combination in 1 turn while adding horizontally bug (2 2 4 8 => 16 : in 1 turn instead of 3)
-# Ensure that movement must occur for an extra 2 to be added to the board
-# README.md
+#   GUI 
+#   Ensure that movement must occur for an extra 2 to be added to the board
+#   README.md
 
 import random
 
@@ -16,16 +13,18 @@ board = [
 ]
 
 def Random_2_Generator():    #Generates a 2 in a random location every turn
-    i = random.randint(0,3)
-    j = random.randint(0,3)
-    if board[i][j] != 0:    #Checks to ensure generated slot is empty
-        Random_2_Generator() 
-    else:
-        board[i][j] = 2
+    empty = []
+    for i in range(0,4): 
+        for j in range(0,4):
+            if board[i][j] == 0: empty += (i,j)     #Finds an empty cell and adds it to empty[]
+    if len(empty) <= 2: board[empty[0]][empty[1]] = 2   #if there is only one empty cell, that cell will be filled
+    else: 
+        x = random.randint(0,(len(empty)-2))    #Else randomly pick a cell and fill it
+        board[empty[x]][empty[x+1]] = 2
 
 Random_2_Generator()    #Generates the first 2 at the start of the game
 
-def Print_Board():  #Prints board to console, Will be removed once GUI is implimented
+def Print_Board():   #Prints board to console, Will be removed once GUI is implimented
     print("\n" + str(board[0][0]) + " | " + str(board[0][1]) + " | " + str(board[0][2]) + " | " + str(board[0][3]))
     print("-   -   -   -")
     print(str(board[1][0]) + " | " + str(board[1][1]) + " | " + str(board[1][2]) + " | " + str(board[1][3]))  
@@ -36,25 +35,15 @@ def Print_Board():  #Prints board to console, Will be removed once GUI is implim
 
 
 def Board_Limits(a):    #Check to make sure numbers dont end up in the -1st or 4th index
-    if a >= 3:
-        a = 3
-    if a <= 0:
-        a = 0
+    if a >= 3: a = 3
+    if a <= 0: a = 0
     return(a)
 
 def Move_Direction(i,j,move):   #Outputs direction that numbers need to move depending on W/A/S/D
-    if move == "w":     #Move Upwards
-        y = i-1
-        x = j
-    elif move =="a":    #Move to the left
-        y = i
-        x = j-1
-    elif move == "s":   #Move Downwards
-        y = i+1
-        x = j
-    elif move == "d":   #Move to the right
-        y = i
-        x = j+1
+    if move == "w": y, x = i-1, j     #Move Upwards
+    elif move == "a": y, x = i, j-1   #Move to the left
+    elif move == "s": y, x = i=1, j #Move Downwards
+    elif move == "d": y, x = i, j+1  #Move to the right
     return(y,x)
 
 def Move_Numbers(move):     #Moves characters in the required direction
@@ -63,7 +52,7 @@ def Move_Numbers(move):     #Moves characters in the required direction
             y, x = Move_Direction(i,j,move)
             x = Board_Limits(x)     #Checks to ensure they are within limits
             y = Board_Limits(y)
-            if board[i][j] != 0 and board[y][x] == 0:   #Checks if cell is empty
+            if board[i][j] != 0 and board[y][x] == 0:    #Checks if cell is empty
                 board[y][x] = board[i][j]
                 board[i][j] = 0
                 Move_Numbers(move)      #Recursively calls itself till all characters have moved
@@ -72,17 +61,27 @@ def Move_Numbers(move):     #Moves characters in the required direction
 def Addition(move):     #Adds characters together once moved
     for i in range(0,4):
         for j in range(0,4):
-            y, x = Move_Direction(i,j,move)
-            if x != Board_Limits(x) or y != Board_Limits(y):    #Check for Limits
-                pass
-            else:  
-                if board[i][j] != 0 and board[y][x] == board[i][j]:     #Checks if the two numbers are equal
+            y, x = Move_Direction(i, j, move)
+            if x != Board_Limits(x) or y != Board_Limits(y): pass    #Check for Limits
+            elif board[i][j] != 0 and board[y][x] == board[i][j]:     #Checks if the two numbers are equal
                     board[i][j] = 0
                     board[y][x] *= 2
 
-while True:
+def Game_over(board):
+    for i in range(0,4):
+        for j in range(0,4):
+            if board[i][j] == 0: return False
+            elif board[i][j] == board[Board_Limits(i+1)][j] or board[i][Board_Limits(j+1)]: return False
+    return True
+
+while Game_over(board) == False:
     Random_2_Generator()
     Print_Board()
     move = input()
-    Move_Numbers(move)
-    Addition(move)
+    if (move == "w" or move == "a" or move == "s" or move == "d"):
+        Move_Numbers(move)
+        Addition(move)
+        Move_Numbers(move)
+    else: pass
+
+print("Game over")
